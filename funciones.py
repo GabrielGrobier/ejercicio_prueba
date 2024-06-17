3c0e73a176e1f9b7a097c37f98f12f15d4a974f0
Gabriel Grobier <ga.grobier@profesor.duoc.cl>
2024-06-17 12:03:42 -0400

    se actualiza comentarios

--- funciones.py
+++ funciones.py
@@ -1,10 +1,16 @@
 
 
-
+#contruccion de funcion agregar productos, donde es una funcion con parametro
+#que recibe la lista productos 
 def agregar_producto(productos):
-    nombre= input("Nombre del producto")
+    #se solicita al usuario el nombre del producto
+    nombre= input("Nombre del producto").lower()
+    #se solicita al usuario la cantidad del producto escrito
     cantidad = int(input("Ingrese la cantidad de producto"))
+    #se solicita el precio 
     precio = int(input("Ingrese el precio del producto"))
+    #con la funcion append se guarda los elementos dentro de la lista en una estructura de
+    #diccionario
     productos.append({
         'nombre':nombre,
         'cantidad':cantidad,
@@ -13,29 +19,39 @@ def agregar_producto(productos):
     print("Productos Guardados ")
     print(productos)
 
-
+#funcion eliminar producto 
 def eliminar_producto(productos):
+    #se solicita el nombre del producto a eliminar 
     nombre = input("ingrese el producto que desea eliminar : ").lower()
+    #se recorre la lista y se guarda los valores de los indices en producto
     for producto in productos:
+        #se comprueba que exista el valor dentro de la lista 
         if producto['nombre'] == nombre:
             print("producto encontrado : ")
             print(f'nombre {producto['nombre']} Cantidad : {producto['cantidad']}')
+            #se elimina el valor encontrado 
             productos.remove(producto)
             print("producto eliminado ")
         
-
+#actualizar cantidad 
 def actualizar_cant(productos):
+    #se solicita el nombre 
     nombre = input("ingrese el producto a actualizar : ").lower()
+    #se ingresa y recorre la lista 
     for producto in productos:
+        #si el nombre escrito por el usuario existe 
         if producto['nombre'] == nombre:
             print(f'nombre {producto['nombre']} Cantidad : {producto['cantidad']}')
+            #se pide la cantidad a actualizar del producto encontrado 
             cantidad = int(input("ingrese la cantidad nueva : "))
+            #se actualiza la cantidad 
             producto['cantidad']= cantidad
             print("producto actualizado ")
             
         
-
+#mostrar productos 
 def Mostrar_invt(productos):
+    #con metodo for se recorre toda la lista y se muestra 
     for producto in productos:
             print(f"Nombre : {producto['nombre']} ")
             print(f"Cantidad : {producto['cantidad']} ")
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+#se traer funciones desde archivo funciones.py
 import funciones as fn
+#contruccion de lista vacia para almacenar datos 
 productos = []
+#menu 
 while True:
     print('''
           1.- Añadir producto 
@@ -10,16 +13,22 @@ while True:
           6.- Salir
           ''')
     op=int(input("Selecciona una opcion : "))
-    
+    #validacion opcion seleccionada por el usuario 
     if op ==1:
+        #llama a funcion agregar producto que esta almacenada en el archivo funciones.py
         fn.agregar_producto(productos)
+    
     if op == 2:
+        #llama a funcion Eliminar_producto que esta almacenada en el archivo funciones.py
         fn.eliminar_producto(productos)
     if op ==3:
+        #llama a funcion actualizar_cant que esta almacenada en el archivo funciones.py
         fn.actualizar_cant(productos)
     if op ==4:
+        #llama a funcion Mostrar_invt que esta almacenada en el archivo funciones.py
         fn.Mostrar_invt(productos)
     if op ==5:
+        #llama a funcion buscar_productos que esta almacenada en el archivo funciones.py
         fn.buscar_productos(productos)
     if op ==6:
         break
